9979bc05f014aeb8e4940a6b7e800773b592e544
phidangnguyen <phidang0210@gmail.com>
2022-04-28 15:17:06 +0700

    Masterchefv2

--- contracts/farming/KaidexMasterChef.sol
+++ contracts/farming/KaidexMasterChef.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./KaidexToken.sol";
+import "../KaidexToken.sol";
 
 interface IMigratorChef {
     function migrate(IERC20 token) external returns (IERC20);
--- test/utilities/index.js
+++ test/utilities/index.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat");
+const { BigNumber } = ethers
 
 
 async function advanceBlock() {
@@ -11,6 +12,41 @@ async function advanceBlockTo(blockNumber) {
   }
 }
 
+const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
+const BASE_TEN = 10
+
+async function deploy(thisObject, contracts) {
+  for (let i in contracts) {
+    let contract = contracts[i]
+    thisObject[contract[0]] = await contract[1].deploy(...(contract[2] || []))
+    await thisObject[contract[0]].deployed()
+  }
+}
+
+function getBigNumber(amount, decimals = 18) {
+  return BigNumber.from(amount).mul(BigNumber.from(BASE_TEN).pow(decimals))
+}
+
+async function prepare(thisObject, contracts) {
+  for (let i in contracts) {
+    let contract = contracts[i]
+    thisObject[contract] = await ethers.getContractFactory(contract)
+  }
+  thisObject.signers = await ethers.getSigners()
+  thisObject.alice = thisObject.signers[0]
+  thisObject.bob = thisObject.signers[1]
+  thisObject.carol = thisObject.signers[2]
+  thisObject.dev = thisObject.signers[3]
+  thisObject.alicePrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+  thisObject.bobPrivateKey = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+  thisObject.carolPrivateKey = "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
+}
+
 module.exports = {
-    advanceBlockTo
+  advanceBlockTo,
+  ADDRESS_ZERO,
+  advanceBlock,
+  deploy,
+  getBigNumber,
+  prepare
 }
