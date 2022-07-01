524ed4d1e763c7ead061a85fd961a1e0623fcabe
phidangnguyen <phidang0210@gmail.com>
2022-07-01 14:30:55 +0700

    Remove capable

--- contracts/KaidexToken.sol
+++ contracts/KaidexToken.sol
@@ -8,30 +8,17 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract KaiDexToken is ERC20Burnable, ERC20VotesComp, Ownable {
-    uint256 private _cap;
 
     constructor(
         string memory name_,
-        string memory symbol_,
-        uint256 cap_
-    ) ERC20(name_, symbol_) Ownable() ERC20Permit(name_) {
-        require(cap_ > 0, "ERC20Capped: cap is 0");
-        _cap = cap_;
-    }
-
-    function cap() public view virtual returns (uint256) {
-        return _cap;
-    }
+        string memory symbol_
+    ) ERC20(name_, symbol_) Ownable() ERC20Permit(name_) {}
 
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
     }
 
     function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
-        require(
-            ERC20.totalSupply() + amount <= cap(),
-            "ERC20Capped: cap exceeded"
-        );
         super._mint(account, amount);
     }
 
