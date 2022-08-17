83561a29cf2411e0fb887aca68b76e2ee2ef73f3
phidangnguyen <phidang0210@gmail.com>
2022-08-17 16:19:05 +0700

    update

--- scripts/raisefund.js
+++ scripts/raisefund.js
@@ -5,13 +5,13 @@
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
-const _buyToken = "0x0000000000000000000000000000000000000000"; // KAI
+const _buyToken = "0xeabd4203d3b3794d336441e4ca6a5d97005e9a70"; // KAI
 const _offeringToken = "0xc4A4fFA90379694B477B04BA66A5feCce5CDdd25";  // KDX
-const _startTime = "1659090000";
-const _endTime = "1659090600";
-const _harvestTime = "1659090600"
+const _startTime = "1659518400";
+const _endTime = "1664784000";
+const _harvestTime = "1664784000"
 const _offeringAmount = "100000000000000000000000";
-const _raisingAmount = "1000000000000000000000";
+const _raisingAmount = "100000000000";
 const _tier = "0x8767d6FF6eb96fFBA0dCDb4927262C05911d1580"
 const _snapshotForm = 1;
 const _snapshotTo = 3
